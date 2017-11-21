b6a2a0438ee26348b9d124eee40624917d1dd830
JessicaMP <jessica95.mp@gmail.com>
2017-11-21 10:34:50 -0500

    aprendiendo

--- js/app.js
+++ js/app.js
@@ -4,12 +4,34 @@ var addList = document.getElementById('addList');
 var form = document.getElementById('form');
 var nameList = document.getElementById('name-list');
 var button = document.getElementById('button-id');
-
+var containerForm = document.getElementsByClassName('container-form')[0];
 function createList(event) {
-  event.target.classList.toggle('display-none');
+  event.preventDefault();
+  event.target.classList.toggle('display-none'); //si tiene la clase se la quita si no la tiene la va agregando.
   form.classList.toggle('display-none');
 }
 
 function begin() {
   addList.addEventListener('click', createList)
+  event.preventDefault();
 }
+
+button.addEventListener('click', function(event) {
+  event.preventDefault();
+  var dad = document.createElement('div');
+  var titleList = document.createElement('p');
+  var brother = document.createElement('a');
+  var reference = document.getElementById('addList');
+  titleList.textContent = nameList.value;
+  brother.setAttribute('href', '#');
+  brother.innerHTML = 'Añadir Lista';
+  //titleList.style.display = 'inline-block';
+  dad.appendChild(titleList);
+  dad.appendChild(brother);
+  dad.classList.add('dad-style');
+  containerForm.appendChild(dad);
+  //form.setAttribute('style', 'visibility:hidden');
+    form.classList.toggle('display-none');
+    addList.classList.toggle('display-none');
+    reference.classList.toggle('display-none');
+})
